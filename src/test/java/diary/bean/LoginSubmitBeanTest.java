b976c8328dacb619ab5b824e21cb0374b2424b84
Ryosuke Tomita <50137312+RyosukeDTomita@users.noreply.github.com>
2023-06-23 21:40:12 +0900

    Revert "Feature/junitworkflow"

--- src/test/java/diary/bean/LoginSubmitBeanTest.java
+++ src/test/java/diary/bean/LoginSubmitBeanTest.java
@@ -21,84 +21,81 @@ import javax.servlet.http.HttpServletResponse;
  * @author iceman
  * @version 1.0
  */
-// public class LoginSubmitBeanTest {
-// /**
-// * Instantiating test class.
-// */
-// public class InstantiateTest {
-// /**
-// * test create {@code LoginSubmitBean} instance.
-// */
-// @Fast
-// @Test
-// public void defaultConstructorTest() {
-// String className = "diary.bean.LoginSubmitBean";
-// try {
-// Class<?> clazz = Class.forName(className); // return "className" Class
-// Object.
-// // newInstance() is deprecaetd.
-// ExecuteInterface usi = (ExecuteInterface)
-// clazz.getDeclaredConstructor().newInstance();
-// } catch (ClassNotFoundException e) {
-// e.printStackTrace();
-// } catch (Exception e) {
-// e.printStackTrace();
-// }
-// }
+public class LoginSubmitBeanTest {
+    /**
+     * Instantiating test class.
+     */
+    public class InstantiateTest {
+        /**
+         * test create {@code LoginSubmitBean} instance.
+         */
+        @Fast
+        @Test
+        public void defaultConstructorTest() {
+            String className = "diary.bean.LoginSubmitBean";
+            try {
+                Class<?> clazz = Class.forName(className); // return "className" Class Object.
+                // newInstance() is deprecaetd.
+                ExecuteInterface usi = (ExecuteInterface) clazz.getDeclaredConstructor().newInstance();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
-// }
+    }
 
-// static ExecuteInterface usi;
+    static ExecuteInterface usi;
 
-// /**
-// * Before test, create {@code LoginSubmitBean} Instance.
-// */
-// @BeforeAll
-// public static void setUpClass() {
-// String className = "diary.bean.LoginSubmitBean";
-// try {
-// Class<?> clazz = Class.forName(className); // return "className" Class
-// Object.
-// // newInstance() is deprecate.
-// usi = (ExecuteInterface) clazz.getDeclaredConstructor().newInstance();
-// } catch (ClassNotFoundException e) {
-// e.printStackTrace();
-// } catch (Exception e) {
-// e.printStackTrace();
-// }
-// }
+    /**
+     * Before test, create {@code LoginSubmitBean} Instance.
+     */
+    @BeforeAll
+    public static void setUpClass() {
+        String className = "diary.bean.LoginSubmitBean";
+        try {
+            Class<?> clazz = Class.forName(className); // return "className" Class Object.
+            // newInstance() is deprecate.
+            usi = (ExecuteInterface) clazz.getDeclaredConstructor().newInstance();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-// /**
-// * This test expect {@code success} pattern.
-// * {@code request}, {@code response} are stub.
-// */
-// @Fast
-// @Test
-// public void executeSuccessTest() {
-// // create request, response stub.
-// HttpServletRequest request = mock(HttpServletRequest.class);
-// when(request.getParameter("loginID")).thenReturn("hoge");
-// when(request.getParameter("password")).thenReturn("password");
-// when(request.getSession(true)).thenReturn(new MockHttpSession());
-// HttpServletResponse response = mock(HttpServletResponse.class);
+    /**
+     * This test expect {@code success} pattern.
+     * {@code request}, {@code response} are stub.
+     */
+    @Fast
+    @Test
+    public void executeSuccessTest() {
+        // create request, response stub.
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getParameter("loginID")).thenReturn("hoge");
+        when(request.getParameter("password")).thenReturn("password");
+        when(request.getSession(true)).thenReturn(new MockHttpSession());
+        HttpServletResponse response = mock(HttpServletResponse.class);
 
-// assertEquals("success", usi.execute(request, response));
-// }
+        assertEquals("success", usi.execute(request, response));
+    }
 
-// /**
-// * This test expect {@code failure} pattern.
-// * {@code request}, {@code response} are stub.
-// */
-// @Fast
-// @Test
-// public void executeFailureTest() {
-// // create request, response stub.
-// HttpServletRequest request = mock(HttpServletRequest.class);
-// when(request.getParameter("loginID")).thenReturn("anonymous");
-// when(request.getParameter("password")).thenReturn("anonymous");
-// HttpServletResponse response = mock(HttpServletResponse.class);
+    /**
+     * This test expect {@code failure} pattern.
+     * {@code request}, {@code response} are stub.
+     */
+    @Fast
+    @Test
+    public void executeFailureTest() {
+        // create request, response stub.
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getParameter("loginID")).thenReturn("anonymous");
+        when(request.getParameter("password")).thenReturn("anonymous");
+        HttpServletResponse response = mock(HttpServletResponse.class);
 
-// assertEquals("failure", usi.execute(request, response));
-// }
+        assertEquals("failure", usi.execute(request, response));
+    }
 
-// }
+}
